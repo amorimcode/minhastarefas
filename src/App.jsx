3567fd56df080be033e294c57c019a1b7a9fdd87
Bruno Amorim <bruno.amorim032@gmail.com>
2021-07-21 20:36:47 -0300

    completed check

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,40 @@
 import React, { useState } from 'react';
+import { v4 as uuidv4 } from 'uuid'
 
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask';
 import './App.css';
 
 const App = () => {
-  const [tasks, setTasks] = useState([
-    {
-      id: '1',
-      title: 'Estudar Programação',
-      completed: false,
-    },
-    {
-      id: '2',
-      title: 'Ler livros',
-      completed: true,
-    },
-    {
-      id: '2',
-      title: 'Ler livros',
-      completed: true,
-    }
-  ]);
+  const [tasks, setTasks] = useState([]);
 
   const handleTaskAddition = (taskTitle) => {
     const newTask = [...tasks, {
       title: taskTitle,
-      id: Math.random(10),
+      id: uuidv4(),
       completed: false,
     }]
 
     setTasks(newTask)
   }
 
+  const handleTaskClick = (taskId) => {
+    const newTasks = tasks.map(task => {
+      if (task.id === taskId) return {
+        ...task, completed: !task.completed
+      }
+      
+      return task
+    })
+
+    setTasks(newTasks)
+  }
+
   return (
     <div>
       <div className="container">
-      <AddTask handleTaskAddition={handleTaskAddition}/>
-      <Tasks tasks={tasks}/>
+        <AddTask handleTaskAddition={handleTaskAddition} />
+        <Tasks tasks={tasks} handleTaskClick={handleTaskClick} />
       </div>
     </div>
   )
